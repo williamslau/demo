d638615d509c776ad70277e97c78bc6af38f08a9
williamslau <williamlaus@outlook.com>
2018-04-27 23:26:01 +0800

    koa2 23fen

--- koa_note/koa_note.js
+++ koa_note/koa_note.js
@@ -32,3 +32,64 @@ app.use((ctx, next) => {
 
 
 // 自己封装koa见koa文件夹
+
+
+
+// koa是洋葱模型，一层套一层
+// 只要调用next或者异步函数的封装就加上await 就加await,防止下一个中间件中有异步
+// 如果不需要继续可以return next() 这样在use中return next()下面的代码就不会执行了
+let Koa = require('koa');
+let app = new Koa();
+
+app.listen(8080);
+function log(){
+    return new Promise(function(resolve,reject){
+        setTimeout(function(){
+            console.log('ok');
+            resolve();
+        },3000);
+    });
+}
+app.use(async (ctx, next) => {
+    console.log(1);
+    await next();
+    console.log(6);
+});
+app.use(async (ctx, next) => {
+    console.log(2);
+    await log();
+    next();
+    console.log(5);
+});
+app.use(async (ctx, next) => {
+    console.log(3);
+    next();
+    console.log(4);
+});
+
+
+
+
+// next原理 合并三个函数的方法
+let fn1 = (ctx, next) => {
+    console.log(1);
+    next();
+    console.log(6);
+}
+let fn2 = (ctx, next) => {
+    console.log(2);
+    next();
+    console.log(5);
+}
+let fn3 = (ctx, next) => {
+    console.log(3);
+    next();
+    console.log(4);
+}
+let fns = [fn1, fn2, fn3];
+function dispatch(index) {
+    let middle = fns[index];
+    if (fns.length === index) return function () { };
+    middle({}, () => dispatch(index + 1))
+}
+dispatch(0);
